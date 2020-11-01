45ce999621f81d1ff3dadf3b908a7f600583423c
Andrew Prokofieff <prokofieff.help@gmail.com>
2020-11-01 22:09:59 +0500

    Added source
    It will delete all messages that contains symbols not from latina alphabet or emoji list

---
+++ application.py
@@ -0,0 +1,38 @@
+import logging
+import string
+import emoji
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '1361020830:AAGRvqqpre8A18EfaAQEQ9UC-8tWCz8sczg'
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+mylist = string.printable
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply(
+        "Hi! I delete a messages that isn't wroten in english letters!\nFollow the rules of the chat!"
+        )
+        
+@dp.message_handler(commands=['test'])
+async def send_welcome(message: types.Message):
+    await message.reply(
+        "Why?"
+        )
+
+
+@dp.message_handler()
+async def send_welcome(message: types.Message):
+    if int(message.chat.id) == -1001279364623:
+        for letter in emoji.demojize(message.text):
+            if letter not in mylist:
+                await bot.delete_message(message.chat.id, message.message_id)
+                await bot.send_message(message.chat.id, "Follow the rules please")
+                break
+    
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
